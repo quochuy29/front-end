3580402ff4716e6e09fbdb0eaf6f2e6decc7cc39
Trương Khánh Ngọc <62151204+khanhngoc2709@users.noreply.github.com>
2021-10-14 14:17:21 +0700

    de-1

---
+++ đề 1/js/script.js
@@ -0,0 +1,22 @@
+$(document).ready(function() {
+
+    $('.header-top-left').click(function(e) {
+        e.preventDefault();
+        $(this).find('.header-title').toggle('show');
+        $(this).siblings().toggle('hide');
+    });
+    
+    $('.web-info-button').click(function(e) {
+        e.preventDefault();
+        $(this).find('.info-content').toggle('show');
+        $(this).siblings().toggle('hide');
+    });
+
+    let navbar = document.querySelector('.navbar-menu');
+    let cover = document.querySelector('.cover');
+    $('.navbar-icon').click(function(e) {
+        e.preventDefault();
+        navbar.classList.toggle('active');
+        cover.classList.toggle('active-cover');
+    });
+});
