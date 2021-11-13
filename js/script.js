0c12e8b96395cb5115803f62a3129f22ed2874fe
{phanquochuy29} <88223974+quochuy29@users.noreply.github.com>
2021-11-13 14:26:39 +0700

    cmt

--- js/script.js
+++ js/script.js
@@ -19,9 +19,20 @@ $(document).ready(function() {
         navbar.classList.toggle('active');
         cover.classList.toggle('active');
     });
+
     $('.cover').click(function(e) {
         e.preventDefault();
         $('.navbar').removeClass('active');
         $('.cover').removeClass('active');
     });
+
+    $('.footer-item .footer-item-title').click(function() {
+        console.log(1)
+        if ($(this).parent().find('.item-detail').attr('class') !== 'item-detail active') {
+            $('.item-detail').removeClass('active');
+            $(this).parent().find('.item-detail').toggleClass('active');
+        } else {
+            $(this).parent().find('.item-detail').toggleClass('active');
+        }
+    })
 });
